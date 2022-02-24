d1fce8cd9b8c47b6cf30a14b42284765f16994e0
InnaKokorina <93093046+InnaKokorina@users.noreply.github.com>
2022-02-24 21:23:38 +0400

    Добавила логику выбора слов по категориям, зачисление баллов за ответы - AliasBrain. Изменила SelectionVC и GuessVC

---
+++ AliasApp/AliasApp/Models/AliasModel.swift
@@ -0,0 +1,13 @@
+//
+//  AliasModel.swift
+//  AliasApp
+//
+//  Created by Inna Kokorina on 24.02.2022.
+//
+
+import Foundation
+
+struct AliasModel {
+    let category: String
+    let word: [String]
+}
