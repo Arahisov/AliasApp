4c0d7f0b6f8cc26d398eff87dbb961cf5f3e0212
mbfuji01 <ltybcjdbx98@gmail.com>
2022-02-24 12:51:08 +0300

    Второе задание

--- AliasApp/AliasApp/ViewControllers/GuessViewController.swift
+++ AliasApp/AliasApp/ViewControllers/GuessViewController.swift
@@ -1,8 +1,30 @@
 import UIKit
 
 class GuessViewController: UIViewController {
-
+    
+    //MARK: - Properties
+    var secondPassed = 0
+    var timer = Timer()
+    
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        progressBar.progress = 0
+        timerStart()
+    }
+    
+    //MARK: - @IBOutlet
+    @IBOutlet weak var progressBar: UIProgressView!
+
+//MARK: - Function 
+    func timerStart() {
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [self] (Timer) in
+            if self.secondPassed <= 60 {
+                progressBar.progress = Float(secondPassed) / Float(60)
+                self.secondPassed += 1
+            } else {
+                Timer.invalidate()
+            }
+        }
     }
 }
